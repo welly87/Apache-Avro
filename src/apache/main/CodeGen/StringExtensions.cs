d92f7d62054dfff91f14a16a60e00028e88c96c8
Welly Tambunan <if05041@gmail.com>
2014-08-09 10:44:39 +0700

    upgrading avro.main to .NET 4.0

---
+++ src/apache/main/CodeGen/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Avro
+{
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                throw new ArgumentException("Input value is null");
+            return input.First().ToString(CultureInfo.InvariantCulture).ToUpper() + String.Join("", input.Skip(1));
+        }
+    }
+}
